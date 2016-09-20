d635b857955ef5954d8e7a3c2b5108421415d5c0
philipcristiano <pc@dev.home.philipcristiano.com>
2016-09-20 19:57:52 +0000

    Working version

--- smf-echo-server.sh
+++ smf-echo-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-$PORT=$(/usr/bin/svcprop -p config/port $SMF_FMRI)
-$FIFO=$(/usr/bin/svcprop -p config/fifo $SMF_FMRI)
-mkfio $FIFO
-exec /usr/bin/nc -kl localhost 2000 > echoserver < echoserver
+PORT=$(/usr/bin/svcprop -p config/port $SMF_FMRI)
+FIFO=$(/usr/bin/svcprop -p config/fifo $SMF_FMRI)
+mkfifo $FIFO
+exec /usr/bin/nc -kl localhost $PORT > echoserver < echoserver
